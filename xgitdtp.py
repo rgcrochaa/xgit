f2e514c140b111c020379734be4e62dcc794b3d8
Rodrigo G. C. Rocha <rgcrocha@gmail.com>
2021-07-26 22:46:11 -0300

    Crawler draft dirs.

---
+++ xgitdtp.py
@@ -0,0 +1,13 @@
+import os
+
+print ("starting")
+
+#def verificar_arquivo(): 
+caminho = "/home/rgcr/development/eclipse-workspace/xgitdatap/files"
+arquivo = caminho
+
+if os.path.exists(arquivo):
+    print ("arquivo existe")
+else:
+        print ("arquivo n existe")
+    
